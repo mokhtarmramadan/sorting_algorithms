84886ee4f66b91fb21733a48a3aa1fe8ee562565
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-10-22 16:25:07 +0300

    Added shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,7 +11,7 @@ void shell_sort(int *array, size_t size)
 {
 	size_t i, j, gap = 1;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size < 2)
 		return;
 
 	for (gap = 1; gap < size / 3;)
